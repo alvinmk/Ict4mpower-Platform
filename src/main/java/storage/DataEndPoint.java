83a65707da4685a7424871a78dea01f39d84ca6d
alvinmk <alvinmk+reg@gmail.com>
2011-10-12 15:58:27 +0200

    The simple dight interface now takes an object and returns it when queried

--- src/main/java/storage/DataEndPoint.java
+++ src/main/java/storage/DataEndPoint.java
@@ -6,15 +6,16 @@ import java.util.Set;
 import storage.dight.MedicalRecord;
 
 public class DataEndPoint {
-	
+	Object o;
 	/*
 	 * This takes an entry and stores it in the database.
 	 * Since it's singned it can no longer be edited. 
 	 */
 	public String SignEntry(Object o,String patientId, long visitId, String app){
 		String type = o.getClass().getName();
+		this.o=o;
 		//Write to dight
-		MedicalRecord mr = new MedicalRecord(o, type, app, patientId, visitId);
+		//MedicalRecord mr = new MedicalRecord(o, type, app, patientId, visitId);
 		return "Id of Entry";
 	}
 	
@@ -24,6 +25,7 @@ public class DataEndPoint {
 	public Set<Object> getEntryFromPatientId(String id){
 		//Make a dight query
 		Set<Object> s = new HashSet<Object>();
+		s.add(o);
 		return s;
 	}
 	
@@ -32,7 +34,9 @@ public class DataEndPoint {
 	 */
 	public Set<Object> getEntriesFromVisitId(long id){
 		//Make a dight query
+		
 		Set<Object> s = new HashSet<Object>();
+		s.add(o);
 		return s;
 	}
 
