dc9aad66fa7feece00a69fae8cbdd81cf0909777
alvinmk <alvinmk+reg@gmail.com>
2011-10-21 09:51:55 +0200

    MeasurmentRecordSocet can now create new measurments and retrive them

--- src/main/java/storage/MeasurementRecordSocket.java
+++ src/main/java/storage/MeasurementRecordSocket.java
@@ -2,13 +2,18 @@ package storage;
 
 import java.util.Set;
 
+import storage.dight.MeasurementRecord;
+
 
 public class MeasurementRecordSocket {
 
-	public Set<Object> getMesurmentByType(String type, String patienId){
-		MeasurementRecordSocket m = new MeasurementRecordSocket();
-		return m.getMesurmentByType(type, patienId);
+	public Set<Object> getMesurmentByType(String measurement, String patienId){
+		MeasurementRecord m = new MeasurementRecord();
+		return m.getMeasurement(patienId, measurement);
 	}
 	
-	
+	public String SignEntry(String measurement, String unit, String value, String patientId){
+		MeasurementRecord m = new MeasurementRecord();
+		return m.newEntry(measurement, unit, value, patientId);
+	}
 }
