23001ec287f53e839e2e9b544232ccaa4b89c2dd
Joakim Lindskog <hem@jordefamn.se>
2011-10-26 16:42:44 +0200

    Changed package names to conform to Java naming conventions.

---
+++ src/main/java/models/AppInfo.java
@@ -0,0 +1,10 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class AppInfo {
+	private ArrayList<HashMap>  visits; //Visits and their status
+	
+}
+ 
---
+++ src/main/java/pagePanels/ClientInfoPanel.java
@@ -0,0 +1,15 @@
+package pagePanels;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+public class ClientInfoPanel extends Panel{
+	private static final long serialVersionUID = -4533918435138308192L;
+	
+	public ClientInfoPanel(String id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+}
